fba7961268bfd23da616c08934a6f35b737fb359
mahboub8061 <mahii.ghafouri@gmail.com>
2023-07-25 15:16:48 +0330

    unit conversion

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 function displayTempreture(response) {
-  let tempretureElement = document.querySelector("#tempreture");
   let humidity = document.querySelector("#Humidity");
+  let tempretureElement = document.querySelector("#tempreture");
+  tempretureElement.innerHTML = Math.round(response.data.main.temp);
   let wind = document.querySelector("#wind");
   let cityElement = document.querySelector("#city");
   let description = document.querySelector("#description");
@@ -9,15 +10,15 @@ function displayTempreture(response) {
   let day = document.querySelector("#day");
   let hours = now.getHours();
   if (hours < 10) {
-    let hours = "0${hours}";
+    let hours = `0${hours}`;
   }
   let minut = now.getMinutes();
   if (minut < 10) {
-    let hours = "0${minut}";
+    let minut = `0${minut}`;
   }
   let timeElement = document.querySelector("#time");
   let icon = document.querySelector("#icon");
-  tempretureElement.innerHTML = Math.round(response.data.main.temp);
+
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
   cityElement.innerHTML = response.data.name;
@@ -30,18 +31,42 @@ function displayTempreture(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].main);
+  // Centigrade to fahrenheit:
+  function ShowTempreture(event) {
+    event.preventDefault();
+    Centigrade.classList.remove("active");
+    fahrenheit.classList.add("active");
+    let temp = document.querySelector("#tempreture");
+    temp.innerHTML = Math.round(response.data.main.temp);
+  }
+  function showFahrenheitTemp(event) {
+    event.preventDefault();
+    let temp = document.querySelector("#tempreture");
+    fahrenheit.classList.remove("active");
+    Centigrade.classList.add("active");
+    temp.innerHTML = Math.round(
+      (Math.round(response.data.main.temp) * 9) / 5 + 32
+    );
+  }
+  let Centigrade = document.querySelector("#centigrade");
+  let fahrenheit = document.querySelector("#fahrenheit");
+  Centigrade.addEventListener("click", ShowTempreture);
+  fahrenheit.addEventListener("click", showFahrenheitTemp);
 }
+
 function search(city) {
   let apiKey = "68c948a4336a2211153a5fdb7bfbe8f9";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTempreture);
 }
 function handleSubmit(event) {
-  //   event.preventdefault();
-  let cityInputElement = document.querySelector("#city-input");
-  console.log(cityInputElement.value);
-  search(cityInputElement.value);
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  let city = cityInput.value;
+  let cityTitle = document.querySelector("#city");
+  cityTitle.innerHTML = city;
+  search(city);
 }
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+search("new york");
