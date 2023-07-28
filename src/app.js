a3d45dd473c103afa98074586eac57955f145c35
mahboub8061 <mahii.ghafouri@gmail.com>
2023-07-28 18:45:55 +0330

    add forecast blocks

--- src/app.js
+++ src/app.js
@@ -6,12 +6,21 @@ function displayTempreture(response) {
   let cityElement = document.querySelector("#city");
   let description = document.querySelector("#description");
   let now = new Date();
-  let Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  let Days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   let day = document.querySelector("#day");
   let hours = now.getHours();
   if (hours < 10) {
     let hours = `0${hours}`;
   }
+  displayForecast();
   let minut = now.getMinutes();
   if (minut < 10) {
     let minut = `0${minut}`;
@@ -48,6 +57,25 @@ function displayTempreture(response) {
       (Math.round(response.data.main.temp) * 9) / 5 + 32
     );
   }
+  function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    let day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+    day.forEach(function (day) {
+      forecastHTML =
+        forecastHTML +
+        ` 
+          <div class="col-2 forecast">
+            <h3 class="forecast-day">${day}</h3>
+            <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII=" alt="">
+            <span class="forecast-temp-max">31°</span><span class="forecast-temp-min">21°</span>
+          </div>
+         `;
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+
   let Centigrade = document.querySelector("#centigrade");
   let fahrenheit = document.querySelector("#fahrenheit");
   Centigrade.addEventListener("click", ShowTempreture);
